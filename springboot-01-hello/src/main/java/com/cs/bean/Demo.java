960ee6c274e0ba898e26d1edf9f35026f54a858f
chengshizhi <5966552@qq.com>
2018-05-13 23:11:44 +0800

    22

---
+++ springboot-01-hello/src/main/java/com/cs/bean/Demo.java
@@ -0,0 +1,22 @@
+package com.cs.bean;
+
+public class Demo {
+	private long id;// 主键.
+	private String name;// 测试名称.
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
